5ec0bc5b19bcacbf87081b1497c6e7769ee37647
zhpanvip <zhapnvip@outlook.com>
2020-01-08 17:16:52 +0800

    TabLayout with ViewPager2

--- app/src/main/java/com/zhpan/sample/viewpager2/activity/VP2FragmentActivity.kt
+++ app/src/main/java/com/zhpan/sample/viewpager2/activity/VP2FragmentActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.integration.testapp.cards.Card
+import com.google.android.material.tabs.TabLayoutMediator
 import com.zhpan.sample.R
 import com.zhpan.sample.viewpager2.fragment.TestFragment
 import kotlinx.android.synthetic.main.activity_fragment.*
@@ -15,7 +16,7 @@ class VP2FragmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
-        view_pager.offscreenPageLimit=1
+//        view_pager.offscreenPageLimit=1
         view_pager.adapter = object : FragmentStateAdapter(this) {
             override fun getItemCount(): Int {
                 return Card.DECK.size
@@ -25,6 +26,13 @@ class VP2FragmentActivity : AppCompatActivity() {
                 return TestFragment.getInstance(Card.DECK[position],position)
             }
         }
+
+        TabLayoutMediator(tab_layout, view_pager) { tab, position ->
+            tab.text = Card.DECK[position].toString()
+        }.attach()
+
+
+
     }
 
 }
