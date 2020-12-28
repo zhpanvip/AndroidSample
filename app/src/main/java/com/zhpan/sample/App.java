c6fa3e010bd2ee81c4cdd259972e9792cde2a297
zhpanvip <zhpanvip@outlook.com>
2020-12-28 14:00:06 +0800

    添加ViewPager2Container.kt，处理VP2滑动冲突.

--- app/src/main/java/com/zhpan/sample/App.java
+++ app/src/main/java/com/zhpan/sample/App.java
@@ -2,9 +2,12 @@ package com.zhpan.sample;
 
 import android.app.Application;
 
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
 import androidx.lifecycle.ProcessLifecycleOwner;
 
-import com.zhpan.sample.lifecycle.AppLifecycleObserver;
+import com.blankj.utilcode.util.ToastUtils;
 
 /**
  * <pre>
@@ -16,6 +19,16 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        ProcessLifecycleOwner.get().getLifecycle().addObserver(new AppLifecycleObserver());
+        ProcessLifecycleOwner.get().getLifecycle().addObserver(new LifecycleObserver() {
+            @OnLifecycleEvent(Lifecycle.Event.ON_START)
+            public void onAppForeground() {
+                ToastUtils.showShort("App已进入前台");
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+            public void onBackground() {
+                ToastUtils.showShort("App已进入后台运行");
+            }
+        });
     }
 }
